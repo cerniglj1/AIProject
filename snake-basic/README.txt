5646f3d778d9bb75c7a72f809675c0591c34ee28
James Cerniglia <cerniglj1@hawkmail.newpaltz.edu>
2018-12-03 22:02:33 -0500

    Implemented Blue path to food along with several other css changes.

--- snake-basic/README.txt
+++ snake-basic/README.txt
@@ -5,9 +5,7 @@ Patches:
 10/14 - Sam made the Snake game structure along with the html page, css page, as well as any other files.
 10/20/18 - Impletemented the ability to choose different search options including BFS, DFS and A*. 
 
-https://github.com/louisbourque/AI-Snake-Game
-Someone else's approach to our situation
-
+https://sacert.github.io/Snake-Star/ 
 
 link to progress report google doc
 https://docs.google.com/document/d/1bVRWSKUNQyOJ4bWADzqPmPej9p6W_D5nvJmDEEN1txM/edit?usp=sharing
--- snake-basic/snake.js
+++ snake-basic/snake.js
@@ -7,16 +7,21 @@ var COLS = 38;
 var BLOCK_W = Math.floor(w / COLS);
 var BLOCK_H = Math.floor(h / ROWS);
 var gameOver = false;
-var tile = 16;
+
+var SearchChosen = "";
+var score = 0;
+var highscore = 0;
+
 // size of grid nxn
 var size = ROWS;
 
-// initialize grid of size 10
+// Initialize grid based on numbers given
 var grid_aStar = grid(size);
 
-// starting values
+// Starts at center of Rows and Columns given
 var start_x = Math.floor(ROWS / 2);
 var start_y = Math.floor(COLS / 2);
+var currentPath = new Array();
 
 // get the starting point of the item (apple)
 do {
@@ -33,6 +38,7 @@ function Node(x, y) {
   this.block = false;
   this.x = x;
   this.y = y;
+  this.path = false;
   this.parent = null;
   this.gScore = -1; // score of getting from start to this node
   this.fScore = -1; // score of gScore plus hueristic value
@@ -101,7 +107,7 @@ function inBoundsCheck(currentNode, i, j) {
   return true;
 }
 
-function A_Star() {
+function aStar() {
   // ending values
   var end_x = item_x;
   var end_y = item_y;
@@ -171,6 +177,140 @@ function A_Star() {
   return false;
 }
 
+function BFS() {
+  // ending values
+  var end_x = item_x;
+  var end_y = item_y;
+
+  // set of nodes that have already been looked at
+  var closedSet = [];
+
+  // set of nodes that are known but not looked at
+  var openSet = [];
+
+  // add the starting element to the open set
+  openSet.push(grid_aStar[start_y][start_x]);
+  grid_aStar[start_y][start_x].gScore = 0;
+  grid_aStar[start_y][start_x].fScore = grid_aStar[start_y][
+    start_x
+  ].heuristicCalc(end_x, end_y); // just the heuristic
+
+  // while open set is not empty
+  while (openSet.length > 0) {
+    openSet.sort(fScoreSort);
+    var currentNode = openSet[0];
+
+    if (currentNode.x == end_x && currentNode.y == end_y) {
+      return reconstruct_path(grid_aStar, currentNode, start_x, start_y); // return path
+    }
+
+    // remove current node from open set
+    var index = openSet.indexOf(currentNode);
+    openSet.splice(index, 1);
+
+    closedSet.push(currentNode);
+
+    // looking at all of the node's neighbours
+    for (var i = -1; i < 2; i++) {
+      for (var j = -1; j < 2; j++) {
+        if (!inBoundsCheck(currentNode, i, j)) {
+          continue;
+        }
+
+        var neighbour = grid_aStar[currentNode.y + i][currentNode.x + j];
+
+        // if node is within the closed set, it has already
+        // been looked at - therefore skip it
+        if (closedSet.indexOf(neighbour) != -1) {
+          continue;
+        }
+
+        // set tentative score to gScore plus distance from current to neighbour
+        // in this case, the weight is equal to 1 everywhere
+        var tScore = neighbour.gScore + 1;
+
+        // if neighbour is not in open set, add it
+        if (openSet.indexOf(neighbour) == -1) {
+          openSet.push(neighbour);
+        }
+
+        // this is a better path so set node's new values
+        neighbour.parent = currentNode;
+        neighbour.gScore = tScore;
+        neighbour.fScore =
+          neighbour.gScore + neighbour.heuristicCalc(end_x, end_y);
+      }
+    }
+  }
+}
+
+function DFS() {
+  // ending values
+  var end_x = item_x;
+  var end_y = item_y;
+
+  // set of nodes that have already been looked at
+  var closedSet = [];
+
+  // set of nodes that are known but not looked at
+  var openSet = [];
+
+  // add the starting element to the open set
+  openSet.push(grid_aStar[start_y][start_x]);
+  grid_aStar[start_y][start_x].gScore = 0;
+  grid_aStar[start_y][start_x].fScore = grid_aStar[start_y][
+    start_x
+  ].heuristicCalc(end_x, end_y); // just the heuristic
+
+  // while open set is not empty
+  while (openSet.length > 0) {
+    openSet.sort(fScoreSort);
+    var currentNode = openSet[0];
+
+    if (currentNode.x == end_x && currentNode.y == end_y) {
+      return reconstruct_path(grid_aStar, currentNode, start_x, start_y); // return path
+    }
+
+    // remove current node from open set
+    var index = openSet.indexOf(currentNode);
+    openSet.splice(index, 1);
+
+    closedSet.push(currentNode);
+
+    // looking at all of the node's neighbours
+    for (var i = -1; i < 2; i++) {
+      for (var j = -1; j < 2; j++) {
+        if (!inBoundsCheck(currentNode, i, j)) {
+          continue;
+        }
+
+        var neighbour = grid_aStar[currentNode.y + i][currentNode.x + j];
+
+        // if node is within the closed set, it has already
+        // been looked at - therefore skip it
+        if (closedSet.indexOf(neighbour) != -1) {
+          continue;
+        }
+
+        // set tentative score to gScore plus distance from current to neighbour
+        // in this case, the weight is equal to 1 everywhere
+        var tScore = neighbour.gScore + 1;
+
+        // if neighbour is not in open set, add it
+        if (openSet.indexOf(neighbour) == -1) {
+          openSet.push(neighbour);
+        }
+
+        // this is a better path so set node's new values
+        neighbour.parent = currentNode;
+        neighbour.gScore = tScore;
+        neighbour.fScore =
+          neighbour.gScore + neighbour.heuristicCalc(end_x, end_y);
+      }
+    }
+  }
+}
+
 function reconstruct_path(grid_aStar, current, start_x, start_y) {
   var currentNode = current;
   var totalPath = [current];
@@ -178,32 +318,45 @@ function reconstruct_path(grid_aStar, current, start_x, start_y) {
   // go through the parents to find how the route
   while (currentNode.parent != null) {
     totalPath.push(currentNode.parent);
+    currentNode.path = true;
     currentNode = currentNode.parent;
   }
 
   return totalPath;
 }
+
 const foodImg = new Image();
 foodImg.src = "imgs/apple.png";
+
 // draws the board and the moving shape
 function draw() {
+  var openSet = [];
+  var tail;
+  document.addEventListener("keydown", direction);
+  openSet.push(grid_aStar[start_y][start_x]);
+
+  var currNode = openSet[0];
   if (!gameOver) {
     for (var x = 0; x < COLS; ++x) {
       for (var y = 0; y < ROWS; ++y) {
         if (y == item_y && x == item_x) {
           // ctx.fillStyle = "red";
-        } else if (snake[0].x == x && snake[0].y == y) {
+        } else if (currNode.x == x && currNode.y == y) {
           ctx.fillStyle = "red";
-          console.log("head");
+          // console.log("head");
+          // } else if (path[y][x].block) {
+          //   ctx.fillStyle = "blue";
+          //   console.log("path");
         } else if (grid_aStar[y][x].block) {
           ctx.fillStyle = "green";
+        } else if (grid_aStar[y][x].path) {
+          ctx.fillStyle = "blue";
         } else {
           ctx.strokeStyle = "black";
-          ctx.lineWidth = "0.8";
+          ctx.lineWidth = "1";
           ctx.fillStyle = "black";
         }
 
-        // ctx.drawImage(boardImg, 0, 0);
         ctx.drawImage(foodImg, item_x * 16, item_y * 16);
         ctx.fillRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W, BLOCK_H);
         ctx.strokeRect(BLOCK_W * x, BLOCK_H * y, BLOCK_W, BLOCK_H);
@@ -291,66 +444,81 @@ function pathLength(currentNode) {
     }
   }
 }
-
 function tick() {
   // keep track of where the trail is
   var tail;
-
+  var path;
   if (!gameOver) {
-    var path = A_Star();
-
-    // clear the grid to perform the next set of calculations
-    for (var j = 0; j < path.length - 1; j++) {
-      path[j].parent = null;
-      path[j].gScore = -1;
-      path[j].fScore = -1;
+    if (SearchChosen == "A*") {
+      path = aStar();
+    } else if (SearchChosen == "BFS") {
+      path = BFS();
+    } else if (SearchChosen == "DFS") {
+      path = DFS();
     }
+  }
 
-    for (var i = 0; i < grid_aStar.length; i++) {
-      for (var j = 0; j < grid_aStar.length; j++) {
-        grid_aStar[i][j].parent = null;
-        grid_aStar[i][j].gScore = -1;
-        grid_aStar[i][j].fScore = -1;
-      }
+  // clear the grid to perform the next set of calculations
+  for (var j = 0; j < path.length - 1; j++) {
+    path[j].parent = null;
+    path[j].gScore = -1;
+    path[j].fScore = -1;
+  }
+
+  for (var i = 0; i < grid_aStar.length; i++) {
+    for (var j = 0; j < grid_aStar.length; j++) {
+      grid_aStar[i][j].parent = null;
+      grid_aStar[i][j].gScore = -1;
+      grid_aStar[i][j].fScore = -1;
     }
+  }
 
-    // if there is a path using A* to the item, go to the first node
-    if (path) {
-      var nextLoc = path[path.length - 2];
+  // if there is a path using A* to the item, go to the first node
+  if (path) {
+    var nextLoc = path[path.length - 2];
+  } else {
+    // otherwise, attempt to find the next best movement
+    var nextNode = getNextMove(item_x, item_y);
+    if (nextNode == null) {
+      gameOver = true;
+      document.getElementById("gameover").innerHTML = "Game Over";
+      return;
     } else {
-      // otherwise, attempt to find the next best movement
-      var nextNode = getNextMove(item_x, item_y);
-      if (nextNode == null) {
-        gameOver = true;
-        document.getElementById("gameover").innerHTML = "Game Over";
-        return;
-      } else {
-        nextLoc = nextNode;
-      }
+      nextLoc = nextNode;
     }
+  }
 
-    // set next location
-    snake.unshift(nextLoc);
-    nextLoc.block = true;
-    start_x = nextLoc.x;
-    start_y = nextLoc.y;
+  // set next location
+  snake.unshift(nextLoc);
+  nextLoc.block = true;
+  start_x = nextLoc.x;
+  start_y = nextLoc.y;
 
-    // if not at the item, pop the tail
-    if (!(nextLoc.x == item_x && nextLoc.y == item_y)) {
-      tail = snake.pop();
-      tail.block = false;
-      tail.gScore = -1;
-      tail.fScore = -1;
-    } else {
-      // if at the item, set a new item location
-      do {
-        item_x = Math.floor(Math.random() * ROWS);
-        item_y = Math.floor(Math.random() * ROWS);
-      } while (grid_aStar[item_y][item_x].block == true);
+  // if not at the item, pop the tail
+  if (!(nextLoc.x == item_x && nextLoc.y == item_y)) {
+    tail = snake.pop();
+    tail.path = false;
+    tail.block = false;
+    tail.gScore = -1;
+    tail.fScore = -1;
+  } else {
+    // if at the item, set a new item location
+    do {
+      item_x = Math.floor(Math.random() * ROWS);
+      item_y = Math.floor(Math.random() * ROWS);
+      grid_aStar[item_y][item_x].path = false;
+    } while (grid_aStar[item_y][item_x].block == true);
+    score++;
+    document.getElementById("score").innerHTML = score;
+    if (score >= highscore) {
+      localStorage.setItem("highscore", score);
+      document.getElementById("highscore").innerHTML = score;
     }
   }
 }
-var dir = "";
+
+var dir = "up";
+
 function direction(event) {
   let key = event.keyCode;
 
@@ -374,24 +542,35 @@ function direction(event) {
 
 function control() {
   // keep track of where the trail is
+  // set of nodes that are known but not looked at
+  var openSet = [];
   var tail;
   document.addEventListener("keydown", direction);
+  openSet.push(grid_aStar[start_y][start_x]);
+
+  var currNode = openSet[0];
+
   if (!gameOver) {
     if (dir == "left") {
-      snake[0].x = snake[0].x - 1;
+      grid_aStar[currNode.y][currNode.x - 1].block = true;
+
+      console.log("left2");
     }
     if (dir == "up") {
-      snake[0].y = snake[0].y - 1;
+      grid_aStar[currNode.y + 1][currNode.x].block = true;
+      console.log("up2");
     }
     if (dir == "right") {
-      snake[0].x = snake[0].x + 1;
+      grid_aStar[currNode.y][currNode.x + 1].block = true;
+      console.log("right2");
     }
     if (dir == "down") {
-      snake[0].y = snake[0].y - 1;
+      grid_aStar[currNode.y - 1][currNode.x].block = true;
+      console.log("down2");
     }
 
     // if not at the item, pop the tail
-    if (!(snake[0].x == item_x && snake[0].y == item_y)) {
+    if (!(currNode.x == item_x && currNode.y == item_y)) {
       tail = snake.pop();
       tail.block = false;
       tail.gScore = -1;
@@ -405,25 +584,33 @@ function control() {
     }
   }
 }
+
 function change_search() {
   var message = new Object();
   message.do = "set_search";
   message.search = document.getElementById("search").value;
   search = message.search;
+  SearchChosen = search;
   console.log("current search: " + message.search);
 }
-var SearchChosen = search;
+
 function start() {
   draw();
-  SearchChosen = search;
-  setInterval(tick, .2);
-  setInterval(draw, 50);
+  if ((highscore = localStorage.getItem("highscore", score) >= 0)) {
+    highscore = localStorage.getItem("highscore", score);
+  } else {
+    highscore = 0;
+  }
+  setInterval(tick, 25);
+  setInterval(draw, 25);
 }
+
 function play() {
   draw();
-  SearchChosen = search;
   setInterval(control, 50);
   setInterval(draw, 50);
 }
+
+change_search();
 
 // startGame();
